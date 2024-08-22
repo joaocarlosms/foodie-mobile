7ebb684db04f595c46c264a2dfb9055d6d9d2475
joaocarlosms <jcmeseverino@gmail.com>
2024-08-22 08:25:28 -0300

    feat: add assets on app

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { styles } from './styles.js'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text style={styles.textHello}>Hello World!</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles.js
@@ -0,0 +1,6 @@
+export const styles = {
+    textHello: {
+        color: "red",
+        fontSize: 50
+    }
+} 
